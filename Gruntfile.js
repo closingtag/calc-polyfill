5cfbf16fd26aa1f3af9c66d7fd185ee24b6d8774
Robert Weber <closingtag@gmail.com>
2015-02-09 15:06:41 +0100

    Added grunt setup
    Signed-off-by: Robert Weber <closingtag@gmail.com>

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+			dist: {
+				files: {
+					'calc.min.js': ['calc.min.js']
+				}
+			}
+		},
+		jshint: {
+			files: ['Gruntfile.js', 'calc.js'],
+			options: {
+				evil: true,
+				// options here to override JSHint defaults
+				globals: {
+					console: true,
+					module: true,
+					document: true
+				}
+			}
+		},
+		includes: {
+			options: {
+				includeRegexp: /^\s*?\/\/\s*import\s+['"]?([^'"]+)['"]?\s*$/,
+				duplicates: false,
+				flatten: false
+			},
+			dist: {
+				files: [
+					{
+					expand: true,
+
+					src: ['calc.js', '!libs/**/*.js'],
+					ext: '.min.js'
+					}
+				]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-includes');
+
+	grunt.registerTask('default', ['jshint', 'includes:dist', 'uglify:dist']);
+
+};
