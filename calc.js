abf4aa24c8804d0f2283bbaf9f0a7a64636e2007
Robert Weber <closingtag@gmail.com>
2015-08-24 20:37:52 +0200

    Load stylesheets when there are only inline styles : fixes #16
    Signed-off-by: Robert Weber <closingtag@gmail.com>

--- calc.js
+++ calc.js
@@ -204,7 +204,7 @@ fillcalc v0.1.0 - (c) Robert Weber, freely distributable under the terms of the 
 		}
 
 
-		if ( stylesheets.length > 0 ) {
+		if ( stylesheets.length > 0 || cssTexts.length > 0 ) {
 
 			loadStylesheets(stylesheets);
 		}
